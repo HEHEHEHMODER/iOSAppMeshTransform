9968f2b02f07a959f75b941767468393df0d0270
Gavin Nelson <gavin@nelson.co>
2025-09-12 22:31:17 -0700

    Initial Commit

---
+++ iOSAppMeshTransform/iOSAppMeshTransformApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSAppMeshTransformApp.swift
+//  iOSAppMeshTransform
+//
+//  Created by Gavin Nelson on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSAppMeshTransformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
